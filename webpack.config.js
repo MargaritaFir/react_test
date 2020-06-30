cf4da59fc4509cccd588e7079acd2b78c9026bfb
MargaritaFir <margaret2009@rambler.ru>
2020-06-30 20:55:28 +0300

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output:{
+        path: path.join(__dirname, "/dist"),
+        filename:"bundle.js"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test:/\.scss/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new webpack.HotModuleReplacementPlugin()
+
+    ],
+    devServer: {
+        contentBase: "./src",
+        port: 7000,
+        hot: true
+    }
+};
